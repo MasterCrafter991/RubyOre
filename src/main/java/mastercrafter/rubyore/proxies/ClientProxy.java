935118ec670125a01e632b3f1a4461510de4ba9d
MasterCrafter991 <mastercrafter991228@gmail.com>
2015-02-22 00:25:28 -0500

    Updating . . .

--- src/main/java/mastercrafter/rubyore/proxies/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-/**
- * 	Used to register sounds and textures that we want to load before our Mod is initialized
- */
-
-package mastercrafter.rubyore.proxies;
-
-public class ClientProxy extends CommonProxy {
-
-}
